ce6354f613c3bb422b578c0ed2fa51fdaa22d94d
Phan Long <long2002tb@gmail.com>
2022-11-07 21:12:28 +0700

    new vim config

---
+++ nvim/lua/plugins/configs/cmp.lua
@@ -0,0 +1,76 @@
+local present, cmp = pcall(require, "cmp")
+
+if not present then
+  return
+end
+
+require("base46").load_highlight "cmp"
+
+vim.o.completeopt = "menuone,noselect"
+
+local function border(hl_name)
+  return {
+    { "╭", hl_name },
+    { "─", hl_name },
+    { "╮", hl_name },
+    { "│", hl_name },
+    { "╯", hl_name },
+    { "─", hl_name },
+    { "╰", hl_name },
+    { "│", hl_name },
+  }
+end
+
+local cmp_window = require "cmp.utils.window"
+
+cmp_window.info_ = cmp_window.info
+cmp_window.info = function(self)
+  local info = self:info_()
+  info.scrollable = false
+  return info
+end
+
+local options = {
+  completion = {
+    completeopt = 'menu,menuone,noinsert',
+ },
+  window = {
+    completion = {
+      border = border "CmpBorder",
+      winhighlight = "Normal:CmpPmenu,CursorLine:PmenuSel,Search:None",
+    },
+    documentation = {
+      border = border "CmpDocBorder",
+    },
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  formatting = {
+    format = function(_, vim_item)
+      local icons = require("nvchad_ui.icons").lspkind
+      vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+      return vim_item
+    end,
+  },
+  mapping = {
+    ["<Tab>"] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+  },
+  sources = {
+    { name = "luasnip" },
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "nvim_lua" },
+    { name = "path" },
+  },
+}
+
+-- check for any override
+options = require("core.utils").load_override(options, "hrsh7th/nvim-cmp")
+
+cmp.setup(options)
